9f9b216e0a9c026becd3b5d778e25570698e64a3
Emily Bowersox <emily.bowersox100@gmail.com>
2020-03-25 12:04:00 -0500

    finished walk-through of for-loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,25 +1,46 @@
 
-// function showMultiplicationTable(aNumber) {
-//     for (var i = 1; i <= 10; i*=10) {
-//         console.log('Function that creates a 1-10 multiplication table for number: '
-//         + aNumber + " * " + i + " = " + showMultiplicationTable());
-//     }
-// }
+function showMultiplicationTable(aNumber) {
+    for (var i = 1; i <= 10; i++) {
+        var answer = aNumber * i;
+        console.log(aNumber + " x " + i + " = " + answer);
+    }
+}
+
+showMultiplicationTable(7);
+
+
+
 //
-// console.log(showMultiplicationTable(7));
-//
+
+for (var i = 0; i < 10; i++) {
+    var randomNumber = Math.floor(Math.random() * 180) + 20;
+
+    if(randomNumber % 2 === 0) {
+        console.log(randomNumber + " is even.");
+    } else {
+        console.log(randomNumber + " is odd.");
+    }
+}
 
 
 //
-// for (var i)  {
-//     console.log('')
-// }
+
+for (var i = 1; i <= 9; i++) {
+    console.log(i.toString().repeat(i));
+}
+
+//alternate, nested loop method
+for (var outer = 1; outer <= 9; outer++) {
+    var output = "";
+
+    for(var inner = 1; inner <= outer; inner++) {
+        output += outer;
+    }
+    console.log(output);
+}
+
 //
-//
-//     Math.floor(Math.random() * 200) + 20
-//
-//
-//
+
 
 
 
