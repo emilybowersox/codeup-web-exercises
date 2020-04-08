8a04db3be02d175c2bf32967c24479acf4d1f4ad
Emily Bowersox <emily.bowersox100@gmail.com>
2020-04-08 15:40:50 -0500

    working with multiple selectors

---
+++ js/jquery_exercises.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// $(document).ready(function () {
+//     alert('The DOM has finished loading');
+// });
+//
+// //"$" references the JQuery object
+// //accessing "documnet" object
+// //event listener/method "ready"
+// //passing and anonymous function (function()) into the event listener "ready"..
+// //..which will execute the code when the document is ready for JS manipulation
+// //the code which is executed in the "alert"
+//
+//
+// var listContents = $('#list').html();
+// alert(listContents);
+//
+//
+//  $('.codeup').css('border', 'solid red 1px');
+
+
+$('li').css('font-size', '20px');
+
+$('h1, p, li').css('background-color', 'grey');
+
