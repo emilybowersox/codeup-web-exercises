82ccec864848b24b13997bb7167db9baf14e31e2
Emily Bowersox <emily.bowersox100@gmail.com>
2020-04-08 16:47:33 -0500

    added styling through jquery

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -14,6 +14,8 @@
 //
 // var listContents = $('#list').html();
 // alert(listContents);
+//or
+// alert($('#list').html());
 //
 //
 //  $('.codeup').css('border', 'solid red 1px');
@@ -22,4 +24,12 @@
 $('li').css('font-size', '20px');
 
 $('h1, p, li').css('background-color', 'grey');
+//not working below
+// $('*').css('font-family', 'sans-serif Arial Ubuntu');
 
+var h1Content = $('h1').html();
+alert(h1Content);
+
+
+//.html() will grab the first of that element referenced
+//but .text() will grab all of it within the multiple of the element
