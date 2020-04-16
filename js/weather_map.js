8aa907d663a3aa7130fc5b2d99abfd14d202f03b
Emily Bowersox <emily.bowersox100@gmail.com>
2020-04-16 16:21:51 -0500

    working on html

--- js/weather_map.js
+++ js/weather_map.js
@@ -7,22 +7,56 @@ weatherMapToken;
 
 
 
-    $.get("http://api.openweathermap.org/data/2.5/weather", {
-        APPID: weatherMapToken,
-        q:     "Dallas, US",
-        units: "imperial"
-    }).done(function (data) {
-        console.log('current weather', data);
-    });
+    function renderHTML(weatherArray){
+        var HTML = "";
+
+        weatherArray.forEach(function (weatherData) {
+            HTML = "<tr>" +
+                "         <td>"+weatherData.name+"</td>" +
+                "         <td>"+weatherData.main.temp+"</td>" +
+                "         <td>"+weatherData.main.temp_max+"</td>" +
+                "         <td>"+weatherData.id+"</td>" +
+                "      </tr>";
+            $("#insertWeather").append(HTML);
+        });
+
+    }
+    //
+    // function getInfo(){
+        $.get("http://api.openweathermap.org/data/2.5/weather", {
+            APPID: weatherMapToken,
+            q:     "Dallas, US",
+            units: "imperial"
+        }).done(function(data){
+            renderHTML(data);
+        }).fail(function(error){
+            console.error(error);
+        });
+    // }
 
 
-    $.get("http://api.openweathermap.org/data/2.5/forecast", {
-        APPID: weatherMapToken,
-        q:     "Dallas, US",
-        units: "imperial"
-    }).done(function (data) {
-        console.log('forecast', data);
-    });
+
+
+    //
+    // $.get("http://api.openweathermap.org/data/2.5/weather", {
+    //     APPID: weatherMapToken,
+    //     q:     "Dallas, US",
+    //     units: "imperial"
+    // }).done(function (data) {
+    //     console.log('current weather', data);
+    // });
+
+
+
+
+
+    // $.get("http://api.openweathermap.org/data/2.5/forecast", {
+    //     APPID: weatherMapToken,
+    //     q:     "Dallas, US",
+    //     units: "imperial"
+    // }).done(function (data) {
+    //     console.log('forecast', data);
+    // });
 
 
 
