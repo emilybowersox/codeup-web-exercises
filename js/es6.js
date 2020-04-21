fafdf27f6281d5e40498badbac9f7bd3ad87ab89
Emily Bowersox <emily.bowersox100@gmail.com>
2020-04-21 15:51:37 -0500

    halway through exercise

--- js/es6.js
+++ js/es6.js
@@ -63,8 +63,8 @@ users.forEach(function(user) {
   return names.push(user.name);
 });
 
-// TODO: replace `var` with `let` in the following declaration
-var developers = [];
+// replace `var` with `let` in the following declaration
+let developers = [];
 users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
@@ -77,8 +77,8 @@ users.forEach(function(user) {
   developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
 });
 
-// TODO: Use `let` for the following variable
-var list = '<ul>';
+// Use `let` for the following variable
+let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
 developers.forEach(function (developer) {
