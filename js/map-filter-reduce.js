47386a355e111e18dabf28ab2fd11749fa163e0a
Emily Bowersox <emily.bowersox100@gmail.com>
2020-04-23 15:39:37 -0500

    added bonus and many notes

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -67,6 +67,7 @@ var anotherFilterLanguages = [];
 anotherFilterLanguages = users.filter(user => user.languages.length >= 3);
 console.log(anotherFilterLanguages);
 
+
 //MAP
 //Use .map to create an array of strings where each element is a user's email address
 var emailAddress = [];
@@ -80,28 +81,87 @@ var moreEmailAddresses = [];
 moreEmailAddresses = users.map(user => user.email);
 console.log(moreEmailAddresses);
 
+
 //REDUCE
 //Use .reduce to get the total years of experience from the list of users.
 // Once you get the total of years you can use the result to calculate the average.
 
-var moreTotalYearsExperience = 0;
-moreTotalYearsExperience = users.reduce(function (total, aUser) {
-    return total + aUser.yearsOfExperience;
-}, 0);
-console.log(moreTotalYearsExperience);
-
-//es6
 var totalYearsExperience = 0;
-//use 0 to initialize a number, '' for strings, {} for objects, [] for arrays.
-totalYearsExperience = users.reduce((total, aUser) => {
+totalYearsExperience = users.reduce(function (total, aUser) {
     return total + aUser.yearsOfExperience;
 }, 0);
 console.log(totalYearsExperience);
+
+//es6
+var moreTotalYearsExperience = 0;
+//use 0 to initialize a number, '' for strings, {} for objects, [] for arrays.
+moreTotalYearsExperience = users.reduce((total, aUser) => {
+    return total + aUser.yearsOfExperience;
+}, 0);
 //do you have to use 'return' for .reduce?
+console.log(moreTotalYearsExperience);
+console.log(users.length);
+
+console.log(moreTotalYearsExperience/users.length);
+//?? how to put the average in a function??...
+
+
+
+// //Use .reduce to get the longest email from the list of users.
+
+var longestEmail = users.reduce((longest, user) => {
+    if (user.email.length > longest.length) {
+        return user.email;
+    } else {
+        return longest
+    }
+}, '');
+console.log(longestEmail)
+//.reduce(accumlation, currentElement)-- the Accumulator and
+// the Current are the parameters for reduce
+//so this function is saying, if the current user email we're
+//currently going over is longer than the length of everything we've
+//seen so far that return it, else, return the one with the longest
+//length of everything we've seen so far
 
 
 
 
+
+//Use .reduce to get the list of user's names in a single string.
+var everyone = users.reduce((names, user) => {
+    return names + ', ' + user.name;
+}, ' ');
+console.log(everyone);
+//gotta specify the inital value for the accumulation (either 0 for
+//numbers or ' ' empty string for strings
+
+
+// Example: Your instructors are: ryan, luis, zach, fernando, justin.
+//
+var instructors = " ",
+    instructors = users.reduce((allInstructors, currentInstructor) => {
+        return allInstructors + ', ' + currentInstructor.name;
+    }, " ");
+console.log(instructors);
+//got it almost- remember accumlator and the currentElement params
+//both need to be represented in the return!!!!
+
+
+
+
+
+//Use .reduce to get the unique list of languages from the list of users.
+const allLanguages = users.reduce((languages, user) => {
+    user.languages.forEach((language) => {
+        if(!languages.includes(language)) {
+            //if you don't already have the language, add it here
+            languages.push(language)
+        }
+    });
+    return languages;
+}, []);
+console.log(allLanguages);
 
 
 
